c30826ba2e0b44cf75c1fdb7885932268277308e
Aparna Padhy <aparnapadhy275@gmail.com>
2026-05-06 19:34:53 +0530

    Updated project architecture and saved ML model

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+import joblib
 
 st.markdown("""
     <style>
@@ -36,10 +35,8 @@ X = data[['area', 'bedrooms', 'bathrooms']]
 y = data['price']
 
 # Train model
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model = LinearRegression()
-model.fit(X_train, y_train)
-
+model = joblib.load("house_price_model.pkl")
+st.success("Saved ML model loaded successfully!")
 st.title("🏡 Housing Price Prediction App")
 
 st.write("Enter property details below:")
